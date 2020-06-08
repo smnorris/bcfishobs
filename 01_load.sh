57bd6442697eb12b5dfaa2f9187a2ba5774c86e2
Simon Norris <snorris@hillcrestgeo.ca>
2020-06-08 15:21:54 -0700

    grab species_cd from my fork for now and use copy instead of ogr2ogr to do the load

--- 01_load.sh
+++ 01_load.sh
@@ -8,12 +8,9 @@
 bcdata bc2pg WHSE_FISH.FISS_FISH_OBSRVTN_PNT_SP \
   --fid FISH_OBSERVATION_POINT_ID
 
-# load 50k waterbody table & species table (not published on DataBC Catalogue)
+# load 50k waterbody table
 wget -N https://hillcrestgeo.ca/outgoing/whse_fish/whse_fish.wdic_waterbodies.csv.zip
-wget -N https://raw.githubusercontent.com/poissonconsulting/fishbc/master/data-raw/whse_fish_species_cd/whse_fish_species_cd.csv
-
 unzip -qjun whse_fish.wdic_waterbodies.csv.zip
-
 ogr2ogr \
   -f PostgreSQL \
   "PG:host=$PGHOST user=$PGUSER dbname=$PGDATABASE port=$PGPORT" \
@@ -22,15 +19,23 @@ ogr2ogr \
   -nln wdic_waterbodies_load \
   -nlt NONE \
   whse_fish.wdic_waterbodies.csv
-
-ogr2ogr \
-  -f PostgreSQL \
-  "PG:host=$PGHOST user=$PGUSER dbname=$PGDATABASE port=$PGPORT" \
-  -lco OVERWRITE=YES \
-  -lco SCHEMA=whse_fish \
-  -nln species_cd \
-  -nlt NONE \
-  whse_fish_species_cd.csv
-
 rm whse_fish.wdic_waterbodies.csv
+
+# load species code table
+wget -N https://raw.githubusercontent.com/smnorris/fishbc/master/data-raw/whse_fish_species_cd/whse_fish_species_cd.csv
+psql -c "DROP TABLE IF EXISTS whse_fish.species_cd;"
+psql -c "CREATE TABLE whse_fish.species_cd
+ (species_id     integer primary key,
+ code            text,
+ name            text,
+ cdcgr_code      text,
+ cdclr_code      text,
+ scientific_name text,
+ spctype_code    text,
+ spcgrp_code     text);"
+
+psql -c "\copy whse_fish.species_cd FROM 'whse_fish_species_cd.csv' delimiter ',' csv header"
+
+
+
 
